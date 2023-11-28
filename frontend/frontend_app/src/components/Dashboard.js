730789d347b0482584852e735abd5d034f800792
Areeba Rashid <areebar2@illinois.edu>
2023-11-27 18:51:52 -0600

    addedall data visualizations to the dashboard

--- frontend/frontend_app/src/components/Dashboard.js
+++ frontend/frontend_app/src/components/Dashboard.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from 'react';
 import './Dashboard.css';
 import Nav from './Navigation';
@@ -19,7 +18,7 @@ function Dashboard() {
     const resizeHandler = () => {
       const resizeViz = (id) => {
         const divElement = document.getElementById(id);
-        const vizElement = divElement?.getElementsByTagName('object')[0];
+        const vizElement = divElement && divElement.getElementsByTagName('object')[0];
         if (vizElement) {
           vizElement.style.width = '100%';
           vizElement.style.height = `${divElement.offsetWidth * 0.75}px`;
@@ -28,6 +27,7 @@ function Dashboard() {
 
       resizeViz('viz1701127565034'); // Resize first visualization
       resizeViz('viz1701127565035'); // Resize second visualization
+      resizeViz('viz1701126787969'); // Resize third visualization
     };
 
     window.addEventListener('resize', resizeHandler);
@@ -77,52 +77,86 @@ function Dashboard() {
                   </a>
                 </noscript>
                 <object className='tableauViz' style={{ display: 'none' }}>
-                  <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-                  <param name='embed_code_version' value='3' />
-                  <param name='site_root' value='' />
-                  <param name='name' value='cs411placedvs_notvisual&#47;Sheet2' />
-                  <param name='tabs' value='no' />
-                  <param name='toolbar' value='yes' />
-                  <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;cs&#47;cs411placedvs_notvisual&#47;Sheet2&#47;1.png' /> 
-                  <param name='animate_transition' value='yes' />
-                  <param name='display_static_image' value='yes' />
-                  <param name='display_spinner' value='yes' />
-                  <param name='display_overlay' value='yes' />
-                  <param name='display_count' value='yes' />
-                  <param name='language' value='en-US' />
-                  <param name='filter' value='publish=yes' />
+                    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+                    <param name='embed_code_version' value='3' /> 
+                    <param name='site_root' value='' />
+                    <param name='name' value='cs411degreedistributionvisual&#47;Sheet3' />
+                    <param name='tabs' value='no' />
+                    <param name='toolbar' value='yes' />
+                    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;cs&#47;cs411degreedistributionvisual&#47;Sheet3&#47;1.png' /> 
+                    <param name='animate_transition' value='yes' />
+                    <param name='display_static_image' value='yes' />
+                    <param name='display_spinner' value='yes' />
+                    <param name='display_overlay' value='yes' />
+                    <param name='display_count' value='yes' />
+                    <param name='language' value='en-US' />
+                    <param name='filter' value='publish=yes' />
                 </object>
               </div>
 
               {/* Second Data Visualization */}
               <div
                 className='tableauPlaceholder'
-                id='viz1701127565035' 
+                id='viz1701127565035'
                 style={{ position: 'relative', width: '100%' }}
               >
-                <noscript>
+               <noscript>
                   <a href='#'>
                     <img
-                      alt='Degree Distribution w/ Gender'
-                      src='https://public.tableau.com/static/images/cs/cs411degreedistributionvisual/Sheet3/1_rss.png'
+                      alt='Sheet2'
+                      src='https://public.tableau.com/views/cs411placedvs_notvisual/Sheet2?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link'
                       style={{ border: 'none' }}
                     />
                   </a>
                 </noscript>
                 <object className='tableauViz' style={{ display: 'none' }}>
-                  <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-                  <param name='embed_code_version' value='3' /> 
-                  <param name='site_root' value='' />
-                  <param name='name' value='cs411degreedistributionvisual&#47;Sheet3' />
-                  <param name='tabs' value='no' /><param name='toolbar' value='yes' />
-                  <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;cs&#47;cs411degreedistributionvisual&#47;Sheet3&#47;1.png' /> 
-                  <param name='animate_transition' value='yes' />
-                  <param name='display_static_image' value='yes' />
-                  <param name='display_spinner' value='yes' />
-                  <param name='display_overlay' value='yes' />
-                  <param name='display_count' value='yes' />
-                  <param name='language' value='en-US' />
-                  <param name='filter' value='publish=yes' />
+                    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+                    <param name='embed_code_version' value='3' /> 
+                    <param name='site_root' value='' />
+                    <param name='name' value='cs411placedvs_notvisual&#47;Sheet2' />
+                    <param name='tabs' value='no' />
+                    <param name='toolbar' value='yes' />
+                    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;cs&#47;cs411placedvs_notvisual&#47;Sheet2&#47;1.png' /> 
+                    <param name='animate_transition' value='yes' />
+                    <param name='display_static_image' value='yes' />
+                    <param name='display_spinner' value='yes' />
+                    <param name='display_overlay' value='yes' />
+                    <param name='display_count' value='yes' />
+                    <param name='language' value='en-US' />
+                    <param name='filter' value='publish=yes' />
+                </object>
+              </div>
+
+              {/* Third Data Visualization */}
+              <div
+                className='tableauPlaceholder'
+                id='viz1701126787969'
+                style={{ position: 'relative', width: '100%' }}
+              >
+                <noscript>
+                  <a href='#'>
+                    <img
+                      alt='Salary Distribution'
+                      src='https://public.tableau.com/static/images/cs/cs411salarydistributionvisual/Sheet1/1_rss.png'
+                      style={{ border: 'none' }}
+                    />
+                  </a>
+                </noscript>
+                <object className='tableauViz' style={{ display: 'none' }}>
+                    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+                    <param name='embed_code_version' value='3' /> 
+                    <param name='site_root' value='' />
+                    <param name='name' value='cs411salarydistributionvisual&#47;Sheet1' />
+                    <param name='tabs' value='no' />
+                    <param name='toolbar' value='yes' />
+                    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;cs&#47;cs411salarydistributionvisual&#47;Sheet1&#47;1.png' /> 
+                    <param name='animate_transition' value='yes' />
+                    <param name='display_static_image' value='yes' />
+                    <param name='display_spinner' value='yes' />
+                    <param name='display_overlay' value='yes' />
+                    <param name='display_count' value='yes' />
+                    <param name='language' value='en-US' />
+                    <param name='filter' value='publish=yes' />
                 </object>
               </div>
             </main>

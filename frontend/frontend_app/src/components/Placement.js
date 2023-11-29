cae7bff041345dfbe2f86f4857011a2e98051bc9
Areeba Rashid <areebar2@illinois.edu>
2023-11-29 14:12:54 -0600

    added form fields

--- frontend/frontend_app/src/components/Placement.js
+++ frontend/frontend_app/src/components/Placement.js
@@ -8,9 +8,33 @@ function Placement() {
         <Nav/>
         <div class="form">
             <form>
-                <label for="fname">First name:</label><br/>
+                <label for="fname">USER_ID:</label><br/>
                 <input type="text" id="fname" name="fname"/><br/>
-                <label for="lname">Last name:</label><br/>
+                <label for="lname">PASSWORD:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">EMAIL:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">NAME:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">HSC_PERCENT:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">HSC_SUBJECT:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">SSC_PERCENT:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">SSC_PERCENT:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">WORK_EXP:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">UNDERGRAD_DEGREE:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">GENDER:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">DEGREE_PERCENT:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">GRAD_PERCENT:</label><br/>
+                <input type="text" id="lname" name="lname"/>
+                <label for="lname">GRAD_DEGREE:</label><br/>
                 <input type="text" id="lname" name="lname"/>
             </form>
         </div>
